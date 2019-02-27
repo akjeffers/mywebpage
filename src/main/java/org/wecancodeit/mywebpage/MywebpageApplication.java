99c524c121c4f81bc46aca8d9ac97489dfc99063
akjeffers <aliciakjeffers@live.com>
2019-02-27 09:58:41 -0500

    css-changes

---
+++ src/main/java/org/wecancodeit/mywebpage/MywebpageApplication.java
@@ -0,0 +1,13 @@
+package org.wecancodeit.mywebpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MywebpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MywebpageApplication.class, args);
+	}
+
+}
